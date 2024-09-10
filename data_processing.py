0e5a9eeb14b2459542e14c133fba6d80527ab876
raul-leal <108129342+raul-leal@users.noreply.github.com>
2024-09-10 20:52:49 +0000

    expansão da função find_similar_books para gênero e autor

--- data_processing.py
+++ data_processing.py
@@ -2,6 +2,8 @@ import pandas as pd
 import nltk
 from nltk.corpus import stopwords as nltk_stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
+# Adição
+from sklearn.metrics.pairwise import cosine_similarity
 import unicodedata
 
 # Carregar stopwords
@@ -24,13 +26,34 @@ def process_dataframe():
     df['normalized_title'] = df['titulo'].apply(normalize_text)
     df['descricao'] = df['descricao'].astype(str).fillna('')
     df['cleaned_description'] = df['descricao'].apply(preprocess)
+    df['normalized_genre'] = df['genero'].apply(normalize_text)
+    df['normalized_author'] = df['autor'].apply(normalize_text)
     return df
 
 # Função para encontrar livros semelhantes usando TF-IDF
-def find_similar_books(df, description, top_n=5):
+def find_similar_books(df, description, genres, author, top_n=5):
+    # Vetorização TF-IDF das descrições
     vectorizer = TfidfVectorizer(stop_words=stop_words_portuguese)
     tfidf_matrix = vectorizer.fit_transform(df['cleaned_description'])
     query_tfidf = vectorizer.transform([preprocess(description)])
+    
+    #Similaridade cosseno das descrições
     cosine_similarities = cosine_similarity(query_tfidf, tfidf_matrix).flatten()
-    similar_indices = cosine_similarities.argsort()[-top_n:][::-1]
+    
+    # Similaridade de gênero (1 se igual, 0 se diferente)
+    genre_similarity = df['normalized_genre'].apply(lambda x: 1 if any(genre in x for genre in genres) else 0)
+
+    #Similaridade de autor (1 se igual, 0 se diferente)
+    author_similarity = df['normalized_author'].apply(lambda x: 1 if x == author else 0)
+
+    # Pesos
+    weight_cosine = 3
+    weight_genre = 2
+    weight_author = 1
+
+    # Similaridade ponderada, futuramente será usada como eixo do histograma
+    weighted_similarity = (weight_cosine * cosine_similarities) + (weight_genre * genre_similarity) + (weight_author * author_similarity)
+
+    # Índices dos livros mais similares
+    similar_indices = weighted_similarity.argsort()[-top_n:][::-1]
     return df.iloc[similar_indices]
